513bd6eeddd0a0b0fc861cf9800837106625bc6a
Shockah <lolshockah@gmail.com>
2012-11-30 19:04:32 +0100

    [Industrial] Energy bar

--- Industrial/Global/World.cs
+++ Industrial/Global/World.cs
@@ -7,6 +7,7 @@ public static void Initialize() {
 }
 
 public static void PreDrawInterface(SpriteBatch sb) {
+	if (ModPlayer.energyMax <= 0) return;
 	int x = Main.screenWidth-300, y = 82;
 	
 	string text = "Energy: "+ModPlayer.energy+"/"+ModPlayer.energyMax;
@@ -15,40 +16,13 @@ public static void PreDrawInterface(SpriteBatch sb) {
 	y += 24;
 	sb.Draw(texBarBorder,new Vector2(x,y),new Rectangle(0,0,texBarBorder.Width,texBarBorder.Height),Color.White);
 	
-	sb.End();
-	
-	DepthStencilState stMask = new DepthStencilState();
-	stMask.StencilEnable = true;
-	stMask.StencilFunction = CompareFunction.Always;
-	stMask.ReferenceStencil = 1;
-	stMask.DepthBufferEnable = true;
-
-	DepthStencilState stTex = new DepthStencilState();
-	stTex.StencilEnable = true;
-	stTex.StencilFunction = CompareFunction.Equal;
-	stTex.ReferenceStencil = 1;
-	stTex.DepthBufferEnable = true;
-	
-	sb.Begin(SpriteSortMode.Immediate,BlendState.AlphaBlend,null,stMask,null);
-	float energyPercent = ModPlayer.energyMax == 0 ? .5f : ModPlayer.energy/ModPlayer.energyMax;
-	
-	Config.mainInstance.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(
-		PrimitiveType.LineStrip,
-		new VertexPositionColor[]{
-			new VertexPositionColor(new Vector3(x+4,y+4,0),Color.White),
-			new VertexPositionColor(new Vector3(x+4+texBar.Width*energyPercent,y+4,0),Color.White),
-			new VertexPositionColor(new Vector3(x+4+texBar.Width*energyPercent-texBar.Height/4,y+4+texBar.Height/2,0),Color.White),
-			new VertexPositionColor(new Vector3(x+4-texBar.Height/4,y+4+texBar.Height/2,0),Color.White)
-		},
-		0,
-		3);
-	sb.End();
-	
-	sb.Begin(SpriteSortMode.Immediate,BlendState.AlphaBlend,null,stTex,null);
-	sb.Draw(texBar,new Vector2(x+4,y+4),new Rectangle(0,0,texBar.Width,texBar.Height),Color.White);
-	sb.End();
-	
-	sb.Begin();
+	int times = (int)Math.Ceiling(texBar.Height/2f);
+	float energyPercent = ModPlayer.energyMax == 0 ? 0f : 1f*ModPlayer.energy/ModPlayer.energyMax;
+	int w = (int)(Math.Floor(texBar.Width/2f*energyPercent)*2);
+	for (int i = 0; i < times; i++) {
+		int ww = w-(i/2)*2;
+		if (ww > 0) sb.Draw(texBar,new Vector2(x+4,y+4+i*2),new Rectangle(0,i*2,ww,2),Color.White);
+	}
 }
 
 public static Color Alpha(Color color, float alpha) {
