634324675d7106b5ff61c6c4f9f0ed52145b4577
shockah <lolshockah@gmail.com>
2013-06-24 19:10:27 +0200

    [StacksUp] Fix for Light Discs

--- Stacks Up!/Item/Item.cs
+++ Stacks Up!/Item/Item.cs
@@ -1,5 +1,5 @@
 public void Initialize() {
-	if (item.maxStack > 1 && !item.name.EndsWith(" Coin")) {
+	if (item.maxStack > 1 && !item.name.EndsWith(" Coin") && item.name != "Light Disc") {
 		if (item.maxStack == 99) item.maxStack = 100;
 		item.maxStack *= ModGeneric.maxStackMultiplier;
 	}
